001489092e5b43fa44389d28e9bbdb477eeb06ab
sagar mochi <chouhansagar131@gmail.com>
2019-07-11 20:22:31 +0530

    updated code

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -24,25 +24,37 @@ def traverseLinked():
     while p is not None:
         print(str(p.info) + ' ' ,end=' ')
         p = p.link
-    return 3
 
 
+def countNodes():
+    p = list.start
+    count = 0
+    while p is not None:
+        count = count + 1
+        p = p.link
+    return count
+
+
+n = 0
 print('press 1 for insert at begg')
 print('press 2 for traverse linked list')
-print('press 3 for exit')
-n = int(input("Press Number:-"))
-while True:
-    print('press 1 for insert at begg')
-    print('press 2 for traverse linked list')
-    print('press 3 for exit')
+print('press 3 for counting the nodes')
+print('press 111 for exit')
+while n is not 111:
+
+    n = int(input("Press Number:-"))
 
     if n == 1:
         value = int(input('Enter Integer value:-'))
         insertAtBeg(value)
         print('value ' + str(value) + ' inseted')
-        n = int(input("Press Number:-"))
     elif n == 2:
-        n = traverseLinked()
+        traverseLinked()
         print()
+        continue
+    elif n == 3:
+        count = countNodes()
+        print('no of nodes in list is=' + str(count))
+        continue
     else:
         break
