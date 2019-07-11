0c9a6e01b6ab2146626d3e9f2bee05445d96f44b
sagar mochi <chouhansagar131@gmail.com>
2019-07-11 19:55:41 +0530

    single linkedlist insetion

---
+++ SingleLinkedList.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self,value):
+        self.info = value
+        self.link = None
+
+class SingleLinked:
+    def __init__(self):
+        self.start = None
+
+
+list = SingleLinked()
+
+def insertAtBeg(value):
+    if list.start == None:
+        list.start = Node(value)
+        list.start.link = None1
+    else:
+        lnk = list.start
+        list.start = Node(value)
+        list.start.link =lnk
+
+def traverseLinked():
+    p = list.start
+    while p is not None:
+        print(str(p.info) + ' ' ,end=' ')
+        p = p.link
+    return 3
+
+
+print('press 1 for insert at begg')
+print('press 2 for traverse linked list')
+print('press 3 for exit')
+n = int(input("Press Number"))
+while True:
+    print('press 1 for insert at begg')
+    print('press 2 for traverse linked list')
+    print('press 3 for exit')
+
+    if n == 1:
+        value = int(input('Enter Integer value:'))
+        insertAtBeg(value)
+        print('value ' + str(value) + ' inseted')
+        n = int(input("Press Number:"))
+    elif n == 2:
+        n = traverseLinked()
+    else:
+        break
