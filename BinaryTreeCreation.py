c2aa03cf708913d3325418398077090c0e537c5c
sagar mochi <chouhansagar131@gmail.com>
2019-08-10 23:55:25 +0530

    binary search tree created

--- BinaryTreeCreation.py
+++ BinaryTreeCreation.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self,data):
-        self.data = data
+        self.info = data
         self.lchild = None
         self.rchild = None
     
@@ -8,10 +8,75 @@ class BinaryTree:
     def __init__(self):
         self.root = None
     
-    def insertTree(self,data):
+    def preorder(self):
+        self.preorderTraver(self.root)
+        print()
+
+    def preorderTraver(self,k):
+        p = k
+        if p is None:
+            return
+        print(p.info,end=" ")
+        self.preorderTraver(p.lchild)
+        self.preorderTraver(p.rchild)
+    
+    def insertTree(self,data=None):
         if self.root is None:
             r = Node(data)
-            root = r
+            self.root = r
         else:
+            p = self.root
+            temp = Node(data)
+            while True:
+                if data < p.info:
+                    while p.lchild is not None and data < p.info:
+                        p = p.lchild
+                    while p.rchild is not None and data > p.info:
+                        p = p.rchild
+                    if data > p.info:
+                        p.rchild = temp
+                        break
+                    else:
+                        p.lchild = temp
+                        break
+
+                else:
+                    while p.lchild is not None and data < p.info:
+                        p = p.lchild
+                    while p.rchild is not None and data > p.info:
+                        p = p.rchild
+                    if data > p.info:
+                        p.rchild = temp
+                        break
+                    else:
+                        p.lchild = temp
+                        break
+        
+        return self.root
+    
+
+
+s = """
+0 for close
+1 for insert continue
+2 for preorder traverse
+"""
+
+b = BinaryTree()
+n = 99
+
+
+while n is not 0:
+    n = int(input(s))
+    if n == 1:
+        data = int(input("ENter Number:"))
+        b.insertTree(data)
+    if n ==2:
+        print("preorder traversing---->")
+        b.preorder()
+        break
+    
+
+
 
 
