c15ee2f55453c213cea22d70c836fa48cb517f3f
sagar mochi <chouhansagar131@gmail.com>
2019-08-26 23:59:57 +0530

    new repo added

---
+++ Math/FindPrimeSieveOfEr.py
@@ -0,0 +1,19 @@
+import math
+n = int(input("ENter N:"))
+lst = [1]*(n+1)
+print(lst)
+
+lst[0] = 0
+lst[1] = 0
+
+for i in range(2,int(math.sqrt(n))+1):
+    if lst[i] == 1:
+        j = 2
+        while i*j <=n:
+            lst[i*j] = 0   # all the multiples of prime  are set to zero whilch is non prime
+            j+=1
+
+print(lst)
+for i,j in enumerate(lst):
+    if j == 1:
+        print(i)
