6da15e27fcd8f1cd950c9821947a80573ac8003d
sagar mochi <chouhansagar131@gmail.com>
2019-08-29 23:41:10 +0530

    pre final commit for this

--- Array/NoCircularRot.java
+++ Array/NoCircularRot.java
@@ -2,7 +2,45 @@ import java.util.Scanner;
 import static java.lang.System.out;
 public class NoCircularRot
 {
-     public static void main(String args[])
+    public static int findRotation(int arr[])
+    {   int res = 0;
+        int n = arr.length;
+        int low=0;
+        int high=n-1;
+        while(true)
+        {
+            int mid = (low+high)/2;
+               
+            if(arr[low] <= arr[high])
+            {
+            
+             res = low;
+             break;
+            }
+            /*
+            need to check it again
+            */
+            
+            int next = (mid+1) % n;
+            int prev = (mid-1+n) % n;
+                
+            if(arr[mid] <= arr[next] && arr[mid] >= arr[prev])
+            {  
+               
+                res = mid;
+                break;
+            }
+                
+            else if(arr[mid] <= arr[high])
+                high = mid-1;
+            else if(arr[mid] >= arr[low])
+                 low =mid+1;    
+        }
+        return res;
+
+    }
+
+    public static void main(String args[])
      {
          Scanner sc = new Scanner(System.in);
          out.println("enter no. array");
@@ -13,6 +51,12 @@ public class NoCircularRot
          {
              arr[i] = sc.nextInt();
          }
+
+        
+         int res = findRotation(arr);
+         out.println("number of rotation is "+res);
+        
+         /*
          int k = 0;
         for(int i=1;i<n;i++)
         {
@@ -20,9 +64,7 @@ public class NoCircularRot
             k = i;
         }
          out.println("Array rotated "+k+" times");
-
-
-
+         */
          sc.close();
      }
 }
